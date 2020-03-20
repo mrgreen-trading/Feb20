66fad9f251786ee5464202c04154c0aa3208bd41
DanielKarama <danielkarama13@gmail.com>
2020-03-20 09:59:17 +0300

    Second commit

--- agent_app/__manifest__.py
+++ agent_app/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Agents App",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module to automatically add agents as well information regarding their payments""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Appskye",
+    'website': "http://www.appskye.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
