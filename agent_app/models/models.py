45738dc718bc19df7b806d00041b68523e9c802d
DanielKarama <danielkarama13@gmail.com>
2020-03-21 06:45:08 +0300

    Added models

--- agent_app/models/models.py
+++ agent_app/models/models.py
@@ -13,3 +13,34 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class AgentModel(models.Model):
+	_name = 'agent_app.agentmodel'
+	_description = "Agents database model"
+
+	name = fields.Char(required=True)
+	phone = fields.Char()
+	location = fields.Char()
+	city_id = fields.Integer()
+	town_id = fields.Integer()
+	mpesa = fields.Boolean()
+	completed = fields.Boolean()
+	direction = fields.Char()
+	latitude = fields.Char()
+	longitude = fields.Char()
+	archive = fields.Boolean()
+	active = fields.Boolean()
+	deleted_at = fields.Datetime()
+	email = fields.Boolean()
+	organized = fields.Boolean()
+	kind = fields.Char()
+	balance = fields.Monetary(compute='_compute_balance')
+
+	@api.multi()
+	def _compute_balance(self, amount,status):
+		for record in self:
+			if status == 'delivered':
+				record.balance +=  amount
+            else:
+			    record.balance -= amount
+	
